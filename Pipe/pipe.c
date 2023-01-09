9a3e734c78082f8aa5cb5618eae2c523ccc1cb47
Noppadon Piya-isarakul <npiya-is@z3t8c1.42bangkok.com>
2023-01-10 00:37:15 +0700

    change pipe to multiple pipe(not fin)

--- Pipe/pipe.c
+++ Pipe/pipe.c
@@ -38,66 +38,84 @@ void	print_cmd(t_cmd *lst_cmd)
 t_cmd	*execute_pipe(t_cmd *cmd)
 {
 	pid_t	process1;
-	pid_t	process2;
-	t_cmd	*second;
-	int		pipefd[4];
+	// pid_t	process2;
+	// t_cmd	*second;
+	// char	buff[1000];
+	int		*pipefd[2];
 	int i; 
-	int		status;
+	// int		status;
 
 	// print_cmd(cmd);
-	if (pipe(pipefd) == -1)
+	pipefd[0] = malloc(2 * sizeof(int));
+	pipefd[1] = malloc(2 * sizeof(int));
+	i = 0;
+	while (i < 2)
 	{
-		write(STDERR_FILENO, "parent: Failed to create pipe\n", 30);
-		exit(1);
+		if (pipe(pipefd[i]) == -1)
+		{
+			write(STDERR_FILENO, "parent: Failed to create pipe\n", 30);
+			exit(1);
+		}
+		i++;
 	}
 	i = 0;
 	while (cmd->next && cmd->next->flag == PIPE)
 	{
-		if (cmd->next->next)
-			second = cmd->next->next;
 		process1 = fork();
 		if (process1 < 0)
 			printf("fork error\n");
 		if (process1 == 0)
 		{
-			close(pipefd[i]);
-			dup2(pipefd[i + 1], STDOUT_FILENO);
-			close(pipefd[i + 1]);
+			// close(pipefd[i][0]);
+			if (cmd->next->next)
+				dup2(pipefd[i][1], STDIN_FILENO);
+			if (i != 0)	
+				dup2(pipefd[i][0], STDOUT_FILENO);
+			// read(pipefd[0], buff, 1000);
+			// printf("buff : %s\n", buff);
+			// close(pipefd[i][1]);
 			if (!assign_pathcmd(cmd, cmd->argv[0]))
 			{
 				perror("command not found");
 				exit(1);
 			}
-			printf("cmd : %s\n", cmd->argv[0]);
 			if (execve(cmd->argv[0], cmd->argv, cmd->env) == -1)
 				printf("kuy1\n");
 		}
-		else
+		// else
+		// {
+			// process2 = fork();
+			// if (process2 < 0)
+			// 	printf("fork2 error\n");
+			// if (process2 == 0)
+			// {
+			// 	waitpid(process1, &status, WNOHANG);
+			// 	close(pipefd[i][1]);
+			// 	dup2(pipefd[i][0], STDIN_FILENO);
+			// 	// read(pipefd[0], buff, 1000);
+			// 	// printf("buff : %s\n", buff);
+			// 	close(pipefd[i][0]);
+			// 	if (!assign_pathcmd(second, second->argv[0]))
+			// 	{
+			// 		perror("command not found");
+			// 		exit(1);
+			// 	}
+			// 	//printf("cmd : %s\n", second->argv[0]);
+			// 	if (execve(second->argv[0], second->argv, second->env) == -1)
+			// 		printf("kuy2\n");
+			// }
+		// }
+		int j = 0;
+		while (j < 2)
 		{
-			process2 = fork();
-			if (process2 < 0)
-				printf("fork2 error\n");
-			if (process2 == 0)
-			{
-				waitpid(process1, &status, WNOHANG);
-				close(pipefd[i + 1]);
-				dup2(pipefd[i], STDIN_FILENO);
-				close(pipefd[i]);
-				if (!assign_pathcmd(second, second->argv[0]))
-				{
-					perror("command not found");
-					exit(1);
-				}
-				printf("cmd : %s\n", second->argv[0]);
-				if (execve(second->argv[0], second->argv, second->env) == -1)
-					printf("kuy2\n");
-			}
+			int k = 0;
+			while (k < 2)
+				close(pipefd[j][k]);
+			j++;
 		}
-		close(pipefd[i]);
-		close(pipefd[i + 1]);
 		// print_cmd(cmd);
 		cmd = cmd->next->next;
-		i += 2;
+		i++;
 	}
 	return (cmd);
 }
